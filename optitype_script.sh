9805a22a85fc1a711d173380cb59c1f8bf5dc33b
John Garza <johnegarza@wustl.edu>
2019-04-15 15:12:02 -0500

    Added optitype option and simplified output file location

--- optitype_script.sh
+++ optitype_script.sh
@@ -52,4 +52,7 @@ rm -f $TEMPDIR/$name.aln.map.rev.sam
 
 echo step 5: run Optitype
 # run optitype 
-python /usr/local/bin/OptiType/OptiTypePipeline.py -i $outdir/$name.hla.fwd.fastq $outdir/$name.hla.rev.fastq --dna -v --outdir $outdir
+python /usr/local/bin/OptiType/OptiTypePipeline.py -i $outdir/$name.hla.fwd.fastq $outdir/$name.hla.rev.fastq --dna -v -p $name -o $outdir
+
+#make sure files actually end up in $outdir and not $outdir/<timestamp>, which is where optitype puts them
+mv $outdir/*/* $outdir
